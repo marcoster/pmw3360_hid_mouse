aaf423a27eef9a1113a35647a4ab599fc2911d6d
Marco Sterbik <madbadmax00@gmail.com>
2021-03-23 16:57:15 +0100

    cleanup

--- PMW3360_HID_Mouse.ino
+++ PMW3360_HID_Mouse.ino
@@ -46,23 +46,30 @@ Module   Arduino
  */
 
 // User define values
-#define SS        10          // Slave Select pin. Connect this to SS on the module.
-#define MOT       17
-#define NUMBTN    5        // number of buttons attached
-#define BTN1      9          // left button pin
-#define BTN2      18          // right button pin
-#define BTN3      20
-#define BTN4      5
-#define BTN5      3
-#define ENCODER_A 6
-#define ENCODER_B 7
-#define DEBOUNCE  10    // debounce itme in ms. Minimun time required for a button to be stabilized.
+#define PMW_SS          10          // Slave Select pin. Connect this to SS on the module.
+#define PMW_MOT         17
+#define NUMBTN          5        // number of buttons attached
+#define BTN_LEFT_NO     9          // left button pin
+#define BTN_LEFT_NC     8
+#define BTN_RIGHT_NO    18          // right button pin
+#define BTN_RIGHT_NC    19
+#define BTN_MIDDLE_NO   20
+#define BTN_MIDDLE_NC   21
+#define BTN_FORWARD_NO  5
+#define BTN_FORWARD_NC  4
+#define BTN_BACK_NO     3
+#define BTN_BACK_NC     2
+#define ENCODER_A       6
+#define ENCODER_B       7
+
+#define PMW_USE_INTERRUPT 0
+#define DEBOUNCE          10
 
 // buttons
-int btn_pins[NUMBTN] = { BTN1, BTN2, BTN3, BTN4, BTN5 };
+int btn_pins[NUMBTN] = { BTN_LEFT_NO, BTN_RIGHT_NO, BTN_MIDDLE_NO, BTN_FORWARD_NO, BTN_BACK_NO };
 char btn_keys[NUMBTN] = { MOUSE_LEFT, MOUSE_RIGHT, MOUSE_MIDDLE, MOUSE_FORWARD, MOUSE_BACK };
 bool btn_state[NUMBTN] = { false, false };
-uint8_t btn_buffers[NUMBTN] = {0xFF, 0xFF};
+uint8_t btn_buffers[NUMBTN] = { 0xFF, 0xFF };
 unsigned long lastButtonCheck = 0;
 
 // sensor
@@ -72,120 +79,124 @@ uint16_t ymax = 0;
 
 // encoder
 RotaryEncoder encoder(ENCODER_A, ENCODER_B, RotaryEncoder::LatchMode::TWO03);
-volatile bool motion = false;
 volatile bool encoderEvent = false;
+#if PMW_USE_INTERRUPT == 1
+volatile bool motion = false;
+#endif
 
-lp22ghost
 
-void setup() {
-  Serial.begin(9600);  
-  
-  // With this line, your arduino will wait until a serial communication begin.
-  // If you want your mouse application to work as soon as plug-in the USB, remove this line.
-  while(!Serial); 
+void setup()
+{
+    Serial.begin(9600);  
+    //while(!Serial);  // remove for immediate operation
 
-  //sensor.begin(10, 1600); // to set CPI (Count per Inch), pass it as the second parameter
-  if(sensor.begin(SS))  // 10 is the pin connected to SS of the module.
-    Serial.println("Sensor initialization successed");
-  else
-    Serial.println("Sensor initialization failed");
-  sensor.setCPI(2000);    // or, you can set CPI later by calling setCPI();
+    // PMW Initialization
+    if(sensor.begin(PMW_SS)) {
+        Serial.println("Sensor initialization successed");
+    } else {
+        Serial.println("Sensor initialization failed");
+    }
+    sensor.setCPI(2000);
 
-  pinMode(MOT, INPUT_PULLUP);
-  //attachInterrupt(digitalPinToInterrupt(MOT), motionDetected, FALLING);
-  //if(digitalRead(MOT) == LOW) {
-  //  motion = true;
-  //}
+#if PMW_USE_INTERRUPT == 1
+    pinMode(PMW_MOT, INPUT_PULLUP);
+    //attachInterrupt(digitalPinToInterrupt(MOT), motionDetected, FALLING);
+    //if(digitalRead(MOT) == LOW) {
+    //  motion = true;
+    //}
+#endif
 
-  attachInterrupt(ENCODER_A, checkPosition, CHANGE);
-  attachInterrupt(ENCODER_B, checkPosition, CHANGE);
-  Mouse.begin();
-  buttons_init();
+    attachInterrupt(ENCODER_A, checkPosition, CHANGE);
+    attachInterrupt(ENCODER_B, checkPosition, CHANGE);
+    Mouse.begin();
+    buttons_init();
 }
 
-void loop() {
-  check_buttons_state();
 
-  //if(motion) {
-  //if(digitalRead(MOT) == LOW) {
-    PMW3360_DATA data = sensor.readBurst(PMW3360_BURST_DATA_MIN_SIZE);
-    if( data.isOnSurface && data.isMotion ) {
-      int mdx = constrain(data.dx, -127, 127);
-      int mdy = constrain(data.dy, -127, 127);
-      
-      Mouse.move(mdx, mdy, 0);
-      if(abs(data.dx) > xmax) {
-        xmax = abs(data.dx);
-      }
-      if(abs(data.dy) > ymax) {
-        ymax = abs(data.dy);
-      }
-      //Serial.printf("%d / %d\n", data.dx, data.dy);
-      //Serial.printf("%d / %d\n", xmax, ymax);
-    }// if
-    //motion = false;
-    //sei();
-  //}
+void loop()
+{
+    check_buttons_state();
+
+#if PMW_USE_INTERRUPT == 1
+    if(motion) {
+#endif
+        PMW3360_DATA data = sensor.readBurst(PMW3360_BURST_DATA_MIN_SIZE);
+        if(data.isOnSurface && data.isMotion) {
+            int mdx = constrain(data.dx, -127, 127);
+            int mdy = constrain(data.dy, -127, 127);
+          
+            Mouse.move(mdx, mdy, 0);
+            if(abs(data.dx) > xmax) {
+                xmax = abs(data.dx);
+            }
+            if(abs(data.dy) > ymax) {
+                ymax = abs(data.dy);
+            }
+            //Serial.printf("%d / %d\n", data.dx, data.dy);
+            //Serial.printf("%d / %d\n", xmax, ymax);
+        }
+#if PMW_USE_INTERRUPT == 1
+        motion = false;
+        sei();
+    }
+#endif
 
 
-  if(encoderEvent == true) {
-    static int pos = 0;
-    int newPos = encoder.getPosition();
-    int dir = (int)encoder.getDirection();
-    encoderEvent = false;
-    //Serial.printf("enc pos/dir: %d/%d\n", pos, dir);
-    Mouse.move(0, 0, (pos - newPos));
-    pos = newPos;
-  }
+    if(encoderEvent == true) {
+        static int pos = 0;
+        int newPos = encoder.getPosition();
+        encoderEvent = false;
+        //Serial.printf("enc pos/dir: %d/%d\n", pos, dir);
+        Mouse.move(0, 0, (pos - newPos));
+        pos = newPos;
+    }
 }
+
 
 void checkPosition()
 {
-  encoder.tick(); // just call tick() to check the state.
-  encoderEvent = true;
+    encoder.tick();
+    encoderEvent = true;
 }
 
-void motionDetected() // interrupt service routine should be minimized.
+#if PMW_USE_INTERRUPT == 1
+void motionDetected()
 {
-  motion = true;    // flag setting.
+    motion = true;
 }
+#endif
+
 
 void buttons_init()
 {
-  for(int i=0;i < NUMBTN; i++)
-  {
-    pinMode(btn_pins[i], INPUT_PULLUP);
-  }
+    for(int i = 0; i < NUMBTN; i++) {
+        pinMode(btn_pins[i], INPUT_PULLUP);
+    }
 }
 
-// Button state checkup routine, fast debounce is implemented.
+
 void check_buttons_state() 
 {
-  unsigned long elapsed = micros() - lastButtonCheck;
+    unsigned long elapsed = micros() - lastButtonCheck;
   
-  // Update at a period of 1/8 of the DEBOUNCE time
-  if(elapsed < (DEBOUNCE * 1000UL / 8))
-    return;
+    if(elapsed < (DEBOUNCE * 1000UL / 8)) {
+        return;
+    }
   
-  lastButtonCheck = micros();
+    lastButtonCheck = micros();
     
-  // Fast Debounce (works with 0 latency most of the time)
-  for(int i=0;i < NUMBTN ; i++)
-  {
-    int state = digitalRead(btn_pins[i]);
-    btn_buffers[i] = btn_buffers[i] << 1 | state; 
+    // Fast Debounce (works with 0 latency most of the time)
+    for(int i=0;i < NUMBTN ; i++) {
+        int state = digitalRead(btn_pins[i]);
+        btn_buffers[i] = btn_buffers[i] << 1 | state; 
 
-    if(!btn_state[i] && btn_buffers[i] == 0xFE)  // button pressed for the first time
-    {
-      Mouse.press(btn_keys[i]);
-      btn_state[i] = true;
+        if(!btn_state[i] && btn_buffers[i] == 0xFE) {  // button pressed for the first time
+            Mouse.press(btn_keys[i]);
+            btn_state[i] = true;
+        } else if( (btn_state[i] && btn_buffers[i] == 0x01) || // button released after stabilized press
+                   (btn_state[i] && btn_buffers[i] == 0xFF) ) { // force release when consequent off state (for the DEBOUNCE time) is detected 
+            Mouse.release(btn_keys[i]);
+            btn_state[i] = false;
+        }
     }
-    else if( (btn_state[i] && btn_buffers[i] == 0x01) // button released after stabilized press
-            // force release when consequent off state (for the DEBOUNCE time) is detected 
-            || (btn_state[i] && btn_buffers[i] == 0xFF) ) 
-    {
-      Mouse.release(btn_keys[i]);
-      btn_state[i] = false;
-    }
-  }
 }
